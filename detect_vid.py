ae2f8241bb634e0cc544f47558ac787abfdc86b1
nishantr05 <66346272+nishantr05@users.noreply.github.com>
2021-02-02 08:07:33 +0530

    Update detect_vid.py

--- detect_vid.py
+++ detect_vid.py
@@ -1,3 +1,4 @@
+from imutils.video import FPS
 import time
 from absl import app, flags, logging
 from absl.flags import FLAGS
@@ -5,37 +6,57 @@ from PIL import Image
 import cv2
 import numpy as np
 
-flags.DEFINE_string('video', './data/video/video.mp4', 'path to input video or set to 0 for webcam')
+flags.DEFINE_string('video', './video.mp4', 'path to input video or set to 0 for webcam')
 flags.DEFINE_string('output', None, 'path to output video')
-flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
+flags.DEFINE_string('output_format', 'mp4v', 'codec used in VideoWriter when saving video to file')
 
 def main(_argv):
    
-    vid = cv2.VideoCapture(video_path)
+    vid = cv2.VideoCapture(FLAGS.video)
     out = None
     if FLAGS.output:
         # by default VideoCapture returns float instead of int
         width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = int(vid.get(cv2.CAP_PROP_FPS))
+        frameps = vid.get(cv2.CAP_PROP_FPS)
         codec = cv2.VideoWriter_fourcc(*FLAGS.output_format)
-        out = cv2.VideoWriter(FLAGS.output, codec, fps, (width, height))
+        out = cv2.VideoWriter(FLAGS.output, codec, frameps, (width, height))
         
-    counter = 0
+    frame_no = 0
+    fps = FPS().start()
+    print("Processing the video...")
     while True:
         return_value, frame = vid.read()
         if return_value:
             img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            #image = Image.fromarray(frame)
+            frame_no += 1
         else:
-            print('Video has ended or failed, try a different video format!')
-            break
+            if frame_no == vid.get(cv2.CAP_PROP_FRAME_COUNT):
+                print("Video processing complete")
+                break
+            raise ValueError("No image! Try with another video format")
     
-        img_new = cv.Laplacian(img, cv.CV_64F)
+        img_new = cv2.Laplacian(img, cv2.CV_64F)
         score = img_new.var()
         threshold = 100
+        #print(fps.type)
 
+        curr_time = float(frame_no)/frameps
+        minutes = int(curr_time/60)
+        seconds = curr_time%60
+        
+        ##Just creating test lables for size of patch
+        label1, label2 = "BlurryFrame", "SharpenessScore=10" 
+        labelSize1, baseLine1 = cv2.getTextSize(label1, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+        labelSize2, baseLine2 = cv2.getTextSize(label2, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+        cv2.rectangle(frame, (5, 15 - labelSize1[1]),
+                              (5 + labelSize1[0], 15 + baseLine1),
+                              (255, 255, 255), cv2.FILLED)
+        cv2.rectangle(frame, (5, 42 - labelSize2[1]),
+                              (5 + labelSize2[0], 42 + baseLine2),
+                              (255, 255, 255), cv2.FILLED)
         if score < threshold:
+            print('Blurry frame detected at {}min {:.1f}sec '.format(minutes, seconds))
             cv2.putText(frame, "Blurry Frame!", (5, 15),
                         cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (200, 0, 0), 2)
             cv2.putText(frame, "Sharpness score = {}".format(score), (5, 20),
@@ -46,12 +67,12 @@ def main(_argv):
             cv2.putText(frame, "Sharpness score = {}".format(score), (5, 20),
                         cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0, 200, 0), 2)
         
-        out.write(result)
-        counter += 1
-      
-        if cv2.waitKey(50) & 0xFF == ord('q'): break
-    #cv2.destroyAllWindows()
+        out.write(frame)
+        fps.update()
 
+    fps.stop()
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 if __name__ == '__main__':
     try:
         app.run(main)
