c03b215454328379d3cb10e1571903c443b94ec6
nishantr05 <66346272+nishantr05@users.noreply.github.com>
2021-02-02 08:36:49 +0530

    Update detect_vid.py

--- detect_vid.py
+++ detect_vid.py
@@ -46,26 +46,28 @@ def main(_argv):
         seconds = curr_time%60
         
         ##Just creating test lables for size of patch
-        label1, label2 = "BlurryFrame", "SharpenessScore=10" 
+        label1, label2 = "BlurryFrame! ", "SharpenesScore=10.00" 
         labelSize1, baseLine1 = cv2.getTextSize(label1, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
         labelSize2, baseLine2 = cv2.getTextSize(label2, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-        cv2.rectangle(frame, (5, 15 - labelSize1[1]),
-                              (5 + labelSize1[0], 15 + baseLine1),
+        y1 = max(15, labelSize1[1])
+        y2 = max(42, labelSize2[1])
+        cv2.rectangle(frame, (5, y1 - labelSize1[1]),
+                              (5 + (labelSize1[0]*3)//2, y1 + baseLine1),
                               (255, 255, 255), cv2.FILLED)
-        cv2.rectangle(frame, (5, 42 - labelSize2[1]),
-                              (5 + labelSize2[0], 42 + baseLine2),
+        cv2.rectangle(frame, (5, y2 - labelSize2[1]),
+                              (5 + (labelSize2[0]*3)//2, y2 + baseLine2),
                               (255, 255, 255), cv2.FILLED)
         if score < threshold:
-            print('Blurry frame detected at {}min {:.1f}sec '.format(minutes, seconds))
+            print('Blurry frame detected at {}min {:.2f}sec '.format(minutes, seconds))
             cv2.putText(frame, "Blurry Frame!", (5, 15),
-                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (200, 0, 0), 2)
-            cv2.putText(frame, "Sharpness score = {}".format(score), (5, 20),
-                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (200, 0, 0), 2)
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0, 0, 200), 1)
+            cv2.putText(frame, "Sharpness score = {:.2f}".format(score), (5, 42),
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0, 0, 200), 1)
         else :
             cv2.putText(frame, "Clear Frame.", (5,15),
-                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0, 200, 0), 2)
-            cv2.putText(frame, "Sharpness score = {}".format(score), (5, 20),
-                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0, 200, 0), 2)
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0, 200, 0), 1)
+            cv2.putText(frame, "Sharpness score = {:.2f}".format(score), (5, 42),
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0, 200, 0), 1)
         
         out.write(frame)
         fps.update()
